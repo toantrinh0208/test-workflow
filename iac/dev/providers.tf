798cc87936f826a4eaf050d2d7c781f565b34770
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2024-02-02 16:38:48 +0700

    Update

--- iac/dev/providers.tf
+++ iac/dev/providers.tf
@@ -6,7 +6,8 @@ provider "aws" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"  # Specify the desired version constraint
     }
   }
 }
